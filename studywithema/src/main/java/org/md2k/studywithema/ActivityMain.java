d1ec4db43e18f58081631c6f07250fa2e379f4fc
Nusrat Nasrin <nusrat27@gmail.com>
2018-02-19 11:36:30 -0600

    MC-943

--- studywithema/src/main/java/org/md2k/studywithema/ActivityMain.java
+++ studywithema/src/main/java/org/md2k/studywithema/ActivityMain.java
@@ -1,6 +1,9 @@
 package org.md2k.studywithema;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import rx.Subscription;
 
@@ -15,7 +18,32 @@ public class ActivityMain extends AbstractActivityMenu {
         if (getIntent().getBooleanExtra("background", false))
             finish();
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_settings, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        Intent intent;
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                finish();
+                break;
+
+            case R.id.action_refresh:
+              //TODO
+         resetDataCollection();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
     public void onDestroy() {
         super.onDestroy();
     }
